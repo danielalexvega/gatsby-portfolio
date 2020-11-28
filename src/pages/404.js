29b462cb82770719b7a9efd8225dac625f6cdde1
Daniel Vega <danielalexvega@gmail.com>
2020-11-28 13:14:58 -0600

    worked on error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,15 @@ import { Link } from "gatsby"
 import SEO from "../components/SEO"
 
 const Error = () => {
-  return <h2>error page</h2>
+  return <Layout>
+    <main className="error-page">
+      <div className="error-container">
+        <h1>My bad.</h1>
+        <h3>This page doesn't exist.</h3>
+        <Link to='/' className='btn'>Go back home.</Link>
+      </div>
+    </main>
+  </Layout >
 }
 
 export default Error
