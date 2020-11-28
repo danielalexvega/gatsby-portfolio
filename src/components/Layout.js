d22bfd5126b04869d97b0982964bc76f6c9a3725
Daniel Vega <danielalexvega@gmail.com>
2020-11-28 10:55:23 -0600

    work on sidebar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 // import "../css/main.css"
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
 
 const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleSideBar = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <>
-      <Navbar />
+      <Navbar toggleSideBar={toggleSideBar} />
+      <Sidebar isOpen={isOpen} toggleSideBar={toggleSideBar} />
       {children}
       <Footer />
     </>
